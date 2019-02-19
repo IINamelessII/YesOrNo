b1fd325c70933e1e77d7d9ba675953d939fce58e
yugisu <slymahel@gmail.com>
2019-02-20 01:35:47 +0200

    add build example

---
+++ frontend/build-example/precache-manifest.585e3569080380bf3b01afa726fcd508.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bda5a8fc65555e724f6e",
+    "url": "/static/js/main.bda5a8fc.chunk.js"
+  },
+  {
+    "revision": "1135100e359357091d3f",
+    "url": "/static/js/2.1135100e.chunk.js"
+  },
+  {
+    "revision": "bda5a8fc65555e724f6e",
+    "url": "/static/css/main.2634107d.chunk.css"
+  },
+  {
+    "revision": "c98b3042f6461836a4c0a083aac3125e",
+    "url": "/index.html"
+  }
+];
