880bd4a8747375ea086dfb68ad8a98fbc651cc76
yugisu <slymahel@gmail.com>
2019-02-19 23:59:34 +0200

    minor changes

--- frontend/src/components/AppBody/GreetingPage/GreetingPage.tsx
+++ frontend/src/components/AppBody/GreetingPage/GreetingPage.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { User } from '../../../types';
+
 import Button from '../../Button';
 import AppBodyHeader from '../AppBodyHeader';
 
 import './GreetingPage.scss';
+import { UserdataContext } from '../../../contexts';
 
 const GreetingPage = () => {
+  const userdata = useContext(UserdataContext);
+
+  const onAuthMessage = userdata.is_auth && ` (hello, ${userdata.username}!)`;
+
+  const addButton = (
+    <Button
+      label="+"
+      style={{ margin: '0 .5rem', display: 'inline-flex', padding: 0 }}
+      flat
+    />
+  );
+
+  const namelessTG = (
+    <a href="https://t.me/IINamelessII" target="__blank">
+      @IINamelessII
+    </a>
+  );
+
+  const yugisuTG = (
+    <a href="https://t.me/inokentich" target="__blank">
+      @inokentich
+    </a>
+  );
+
   return (
     <div className="greeting-page">
       <AppBodyHeader label="Welcome to YesOrNo alpha v0.666!" />
       <div className="greeting-page__content">
         <p>
-          Each alpha-tester is recommended to register / sign in and
-          <Button
-            label="+"
-            style={{ margin: '0 .5rem', display: 'inline-flex', padding: 0 }}
-            flat
-          />
-          at least 5 polls distributed among any flows and also vote in polls
-          created by other users.
+          Each alpha-tester {onAuthMessage} is recommended to register / sign in
+          and {addButton} at least 5 polls distributed among any flows and also
+          vote in polls created by other users.
         </p>
         <p>
           To start voting choose Flow or just push "Random!" button in left
@@ -26,18 +49,9 @@ const GreetingPage = () => {
         <p>
           Report problems found to
           <span className="text--accent"> theyesornoproject@gmail.com</span> or
-          to us in Telegram:
-          <a href="https://t.me/IINamelessII" target="__blank">
-            @IINamelessII
-          </a>
-          ,
-          <a href="https://t.me/inokentich" target="__blank">
-            @inokentich
-          </a>
-          .
+          to us in Telegram: {namelessTG}, {yugisuTG}.
         </p>
-        <p>Have a nice time!</p>
-        <p>IINamelessII, yugisu</p>
+        <p>Have a nice time! With best wishes, IINamelessII & yugisu.</p>
       </div>
     </div>
   );
