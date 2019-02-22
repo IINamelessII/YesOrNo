2a910d98d176e754ee0afd1ac628fd20468b069e
IINamelessII <justnitro26@gmail.com>
2019-02-22 12:13:41 +0200

    Fix the static problem, remove meta tag with csrf

---
+++ frontend/precache-manifest.67806c166f29af5cac8212a8e029040b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1ec4f21d07fabcf2b304",
+    "url": "/static/js/main.1ec4f21d.chunk.js"
+  },
+  {
+    "revision": "35d1f9554af84c949d52",
+    "url": "/static/js/2.35d1f955.chunk.js"
+  },
+  {
+    "revision": "1ec4f21d07fabcf2b304",
+    "url": "/static/css/main.9264ef3a.chunk.css"
+  },
+  {
+    "revision": "18cc3168fd16a4a55f841cf457dbeaf8",
+    "url": "/index.html"
+  }
+];
