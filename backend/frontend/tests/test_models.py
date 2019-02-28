b7caa4b29cd90c1a8e24f9ecc56cb31b0f242d01
IINamelessII <justnitro26@gmail.com>
2019-02-28 18:55:50 +0200

    Add tests for unvoteYes, unvoteNo, unrateLike, unrateDislike Profile methods

--- backend/frontend/tests/test_models.py
+++ backend/frontend/tests/test_models.py
@@ -80,3 +80,71 @@ class TestProfile(TestCase):
         poll_id = randint(1, 2147483647)
         self.model.rateDislike(poll_id)
         self.assertEquals(self.model.rated[poll_id], False)
+
+    def test_unvoteYes_voted_was_true(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = True
+        self.model.unvoteYes(poll_id)
+        self.assertEquals(self.model.voted.get(poll_id), None)
+
+    def test_unvoteYes_voted_was_false(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = False
+        self.model.unvoteYes(poll_id)
+        self.assertEquals(self.model.voted.get(poll_id), None)
+
+    def test_unvoteYes_voted_wasnt(self):
+        poll_id = randint(1, 2147483647)
+        self.model.unvoteYes(poll_id)
+        self.assertEquals(self.model.voted.get(poll_id), None)
+
+    def test_unvoteNo_voted_was_true(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = True
+        self.model.unvoteNo(poll_id)
+        self.assertEquals(self.model.voted.get(poll_id), None)
+
+    def test_unvoteNo_voted_was_false(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = False
+        self.model.unvoteNo(poll_id)
+        self.assertEquals(self.model.voted.get(poll_id), None)
+
+    def test_unvoteNo_voted_wasnt(self):
+        poll_id = randint(1, 2147483647)
+        self.model.unvoteNo(poll_id)
+        self.assertEquals(self.model.voted.get(poll_id), None)
+
+    def test_unrateLike_rated_was_true(self):
+        poll_id = randint(1, 2147483647)
+        self.model.rated[poll_id] = True
+        self.model.unrateLike(poll_id)
+        self.assertEquals(self.model.rated.get(poll_id), None)
+
+    def test_unrateLike_rated_was_false(self):
+        poll_id = randint(1, 2147483647)
+        self.model.rated[poll_id] = False
+        self.model.unrateLike(poll_id)
+        self.assertEquals(self.model.rated.get(poll_id), None)
+
+    def test_unvrateLike_rated_wasnt(self):
+        poll_id = randint(1, 2147483647)
+        self.model.unrateLike(poll_id)
+        self.assertEquals(self.model.rated.get(poll_id), None)
+
+    def test_unrateDislike_rated_was_true(self):
+        poll_id = randint(1, 2147483647)
+        self.model.rated[poll_id] = True
+        self.model.unrateDislike(poll_id)
+        self.assertEquals(self.model.rated.get(poll_id), None)
+
+    def test_unvrateDislike_rated_was_false(self):
+        poll_id = randint(1, 2147483647)
+        self.model.rated[poll_id] = False
+        self.model.unrateDislike(poll_id)
+        self.assertEquals(self.model.rated.get(poll_id), None)
+
+    def test_unrateDislike_rated_wasnt(self):
+        poll_id = randint(1, 2147483647)
+        self.model.unrateDislike(poll_id)
+        self.assertEquals(self.model.rated.get(poll_id), None)
