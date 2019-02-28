36fc526f4ecb1cac427ba352bf554bb6464c8644
IINamelessII <justnitro26@gmail.com>
2019-02-28 18:06:43 +0200

    Fix TestProfile's setUp method and add tests for voteYes and voteNo Profile methods

--- backend/frontend/tests/test_models.py
+++ backend/frontend/tests/test_models.py
@@ -1,20 +1,49 @@
 from random import randint
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.test import TestCase
 from model_mommy import mommy
+from frontend.models import Profile
 
 
-# class TestProfile(TestCase):
-#     def setUp(self):
-#         self.user_model = mommy.make('User')
-#         self.model = mommy.make('frontend.Profile', user=self.user_model)
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user_model = mommy.make('User')
+        self.model = Profile.objects.get(user=self.user_model)
     
-#     def test_str(self):
-#         self.assertEquals(str(self.model), self.model.user.username)
+    def test_str(self):
+        self.assertEquals(str(self.model), self.model.user.username)
 
-    # def test_voteYes(self):
-    #     poll_id = randint(1, 2147483647)
-    #     self.voted[poll_id] = False
-    #     self.save()
-    #     self.model.voteYes(poll_id)
-    #     self.assertEquals(self.model.voted[poll_id], True)
+    def test_voteYes_voted_was_false(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = False
+        self.model.voteYes(poll_id)
+        self.assertEquals(self.model.voted[poll_id], True)
+
+    def test_voteYes_voted_was_true(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = True
+        self.model.voteYes(poll_id)
+        self.assertEquals(self.model.voted[poll_id], True)
+    
+    def test_voteYes_voted_wasnt(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voteYes(poll_id)
+        self.assertEquals(self.model.voted[poll_id], True)
+    
+    def test_voteNo_voted_was_false(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = False
+        self.model.voteNo(poll_id)
+        self.assertEquals(self.model.voted[poll_id], False)    
+    
+    def test_voteNo_voted_was_true(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voted[poll_id] = True
+        self.model.voteNo(poll_id)
+        self.assertEquals(self.model.voted[poll_id], False)
+
+    def test_voteNo_voted_wasnt(self):
+        poll_id = randint(1, 2147483647)
+        self.model.voteNo(poll_id)
+        self.assertEquals(self.model.voted[poll_id], False)
+    
