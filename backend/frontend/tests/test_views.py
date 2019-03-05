8a565fe09713319067735be7521ef4a3a6b59b42
IINamelessII <justnitro26@gmail.com>
2019-03-05 11:35:52 +0200

    Minor changes in tests

--- backend/frontend/tests/test_views.py
+++ backend/frontend/tests/test_views.py
@@ -1,9 +1,13 @@
 from importlib import import_module
+from json import dumps
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.test import TestCase
 from model_mommy import mommy
 from rest_framework.test import APIRequestFactory, force_authenticate
 from frontend import views
+from frontend.models import Profile
+from polls.models import Poll
 
 
 class TestIndexView(TestCase):
@@ -18,7 +22,7 @@ class TestLogoutView(TestCase):
         self.factory = APIRequestFactory()
         self.user_model = mommy.make('User')
 
-    def test_logout_user_was_authenticated(self):
+    def test_user_was_authenticated(self):
         request = self.factory.get('/')
         engine = import_module(settings.SESSION_ENGINE)
         session_key = None
@@ -27,7 +31,7 @@ class TestLogoutView(TestCase):
         response = self.view(request)
         self.assertEquals(response.status_code, 200)
 
-    def test_logout_user_wasnt_authenticated(self):
+    def test_user_wasnt_authenticated(self):
         request = self.factory.get('/')
         engine = import_module(settings.SESSION_ENGINE)
         session_key = None
@@ -35,8 +39,28 @@ class TestLogoutView(TestCase):
         response = self.view(request)
         self.assertEquals(response.status_code, 200)
 
-    def test_logout_user_wasnt_authenticated_without_session(self):
+    def test_user_wasnt_authenticated_without_session(self):
         request = self.factory.get('/')
         response = self.view(request)
         self.assertEquals(response.status_code, 500)
-    
+
+
+class TestVoteYesView(TestCase):
+    def setUp(self):
+        self.view = views.voteYes
+        self.poll_model = mommy.make('polls.Poll')
+        self.user_model = mommy.make('User')
+        self.profile_model = Profile.objects.get(user=self.user_model)
+        self.factory = APIRequestFactory()
+        
+    def test(self):
+        agree = self.poll_model.agree
+        self.profile_model.voted[self.poll_model.pk] = False
+        request = self.factory.post('/', dumps({'id': self.poll_model.pk}), content_type='application/json')
+        force_authenticate(request, user=self.user_model)
+        response = self.view(request)
+        self.poll_model.refresh_from_db()
+        new_agree = agree + 1
+        self.assertEquals(new_agree, self.poll_model.agree)
+        self.assertEquals(self.profile_model[self.poll_model.pk], True)
+
