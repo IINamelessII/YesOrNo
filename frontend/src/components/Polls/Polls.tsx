87a20b1fb77da3f1692f62c303d633eeed632c32
yugisu <slymahel@gmail.com>
2019-03-27 22:21:28 +0200

    minor structure optimization

--- frontend/src/components/Polls/Polls.tsx
+++ frontend/src/components/Polls/Polls.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { usePolls } from './hooks';
+import { UserdataContext } from '../../contexts';
 
 import Spinner from '../Spinner';
-import PollsView from './PollsView';
+import NewPoll from './NewPoll';
+import Poll from './Poll/Poll';
+
+import './Polls.scss';
 
 export type Props = {
   selectedFlow: string;
 };
 
 const Polls = ({ selectedFlow }: Props) => {
+  const { userdata } = useContext(UserdataContext);
   const { pollData, loading, addPoll, error } = usePolls(selectedFlow);
 
-  return loading ? (
-    <Spinner mimicClass="polls" />
-  ) : (
-    <PollsView
-      pollData={pollData}
-      selectedFlow={selectedFlow}
-      addPoll={addPoll}
-    />
-  );
+  if (loading) return <Spinner mimicClass="polls" />;
+
+  if (!userdata.is_auth && pollData.length === 0) {
+    return (
+      <span className="polls__empty-message" key="poll-empty">
+        Whoops! No polls here! <span className="text--accent">Sign in</span> to
+        add the first one!
+      </span>
+    );
+  }
+
+  const pollElements = pollData.map(({ poll, voteRateData, voteFunctions }) => {
+    return (
+      <Poll
+        poll={poll}
+        key={`poll-${poll.id}`}
+        is_auth={userdata.is_auth}
+        {...voteRateData}
+        {...voteFunctions}
+      />
+    );
+  });
+
+  if (userdata.is_auth) {
+    pollElements.unshift(
+      <NewPoll key="poll-new" selectedFlow={selectedFlow} addPoll={addPoll} />
+    );
+  }
+
+  return <section className="polls">{pollElements}</section>;
 };
 
 export default Polls;
