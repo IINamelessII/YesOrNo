7685b3dffbaa7e60fcb5e4c4d994cfa3ae9125e4
IINamelessII <justnitro26@gmail.com>
2019-04-02 17:33:40 +0300

    Add tests for PollByUserList

--- backend/polls/tests/test_views.py
+++ backend/polls/tests/test_views.py
@@ -18,12 +18,12 @@ class TestPollByFlowNameList(TestCase):
         polls = Poll.objects.filter(flow__name=self.flow_model.name)
         self.assertQuerysetEqual(view.get_queryset(), map(repr, polls))
     
-    def test_get_queryser_empty(self):
+    def test_get_queryset_empty(self):
         view = self.view(kwargs={'flow_name': ''})
         polls = Poll.objects.filter(flow__name='')
         self.assertQuerysetEqual(view.get_queryset(), map(repr, polls))
     
-    def test_get_queryser_fail(self):
+    def test_get_queryset_fail(self):
         view = self.view(kwargs={'not_a_flow_name': 'some'})
         self.assertEquals(view.get_queryset().status_code, 404)
 
@@ -32,18 +32,27 @@ class TestPollByUserList(TestCase):
     def setUp(self):
         self.user_model = mommy.make('User')
         self.flow_model = mommy.make('Flow')
-        self.factory = APIRequestFactory()
-        self.view = PollByUserList.get_queryset
-
+        self.view = PollByUserList
+        
+        class Request():
+            def __init__(request_self):
+                request_self.user = self.user_model
+        
+        self.request = Request()
+    
     def test_get_queryset(self):
-        request = self.factory.get('/')
-        engine = import_module(settings.SESSION_ENGINE)
-        session_key = None
-        request.session = engine.SessionStore(session_key)
-        force_authenticate(request, user=self.user_model)
-        poll_by_user = Poll(owner=self.user_model, statement='Just a test?', flow=self.flow_model)
-        poll_by_user.save()
+        poll = Poll(owner=self.user_model, statement='Just a test?', flow=self.flow_model)
+        poll.save()
+        view = self.view(request=self.request)
         polls = Poll.objects.filter(owner=self.user_model)
-        self.assertEquals(self.view(request), polls)
-        # self.assertEquals(request.user, self.user_model)
-        # self.assertQuerysetEqual(view.get_queryset(), map(repr, polls))
+        self.assertQuerysetEqual(view.get_queryset(), map(repr, polls))
+
+    def test_get_queryset_empty(self):
+        view = self.view(request=self.request)
+        polls = Poll.objects.filter(owner=self.user_model)
+        self.assertQuerysetEqual(view.get_queryset(), map(repr, polls))
+
+    def test_get_queryset_fail(self):
+        view = self.view()
+        polls = Poll.objects.filter(owner=self.user_model)
+        self.assertEquals(view.get_queryset(), None)
